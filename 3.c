dd13da121035f0e1b264bbf06b6aed59b20add03
FedeBGitHub <fedebarrionuevo76@gmail.com>
2024-04-23 11:14:48 -0300

    Punto 3 realizado

---
+++ 3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char buff[100];
+    char ** nombres = malloc(5 * sizeof(char *));;
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\n Ingrese un nombre: ");
+        scanf("%s", buff);
+        
+        nombres[i] = malloc((strlen(buff) + 1) * sizeof(char));
+        strcpy(nombres[i], buff);
+    }
+
+
+    for (int j = 0; j < 5; j++)
+    {
+    
+        printf("\n%s", nombres[j]);
+        free(nombres[j]);
+    }
+    free(nombres);
+
+    return 0;
+}
